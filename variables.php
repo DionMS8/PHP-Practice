fa52eb3e50d092143f23c617322f794430d16b91
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-02-24 21:15:06 -0400

    Covered Variables, Functions, Strings, and Arrays

---
+++ variables.php
@@ -0,0 +1,59 @@
+//=====[VARIABLES]======================================
+
+//=====[DECLARING VARIABLES]===========================================
+
+$name = "Dion";
+$age = 28;
+$isMale = true; 
+$height = 1.85;
+$salary = null;
+
+//=====[PRINTING VARIABLES]===========================================
+
+echo $name . '<br>';
+echo $age . '<br>';
+echo $isMale . '<br>';
+echo $height . '<br>';
+echo $salary . '<br>';
+
+//=====[PRINTING VARIABLE TYPES]===========================================
+
+echo gettype($name) . '<br>';
+echo gettype($age) . '<br>';
+echo gettype($isMale) . '<br>';
+echo gettype($height) . '<br>';
+echo gettype($salary) . '<br>';
+
+//=====[CHECKING VARIABLE TYPES]===========================================
+
+is_string($name); // false
+is_int($age); // true
+is_bool($isMale); // true
+is_double($height); // true
+
+// Check if variable is defined
+
+var_dump(isset($name));
+var_dump(isset($name2));
+echo '<br>';
+
+// Constants
+
+define('PI', 3.14);
+echo PI.'<br>';
+var_dump(defined('PI')); // defined(PI2)
+echo '<br>';
+
+//=====[PHP BUILT-IN CONSTANTS]================================================
+
+echo SORT_ASC.'<br>';
+echo PHP_INT_MAX.'<br>';
+
+
+
+
+
+
+
+
+
